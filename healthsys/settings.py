97334833384ca9e2d307cd2eec9eff50328a93f0
xinyiidan <1540907926@qq.com>
2023-01-19 16:10:56 +0800

    更改数据库引擎为mysql

--- healthsys/settings.py
+++ healthsys/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'healthsys.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'healthsys',
+        'USER': 'root',
+        'PASSWORD': os.environ.get("MY_DB_PASS"),
+        'HOST': 'localhost',
+        'POST': '3306'
     }
 }
 
@@ -105,7 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
